e7a0e096ddc5927c47b5dcad0e695053a21f7004
Nikhil Nawgiri <the@realest.ninja>
2021-12-11 19:57:27 +0100

    Refactor

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import AppController from "./controllers/AppController";
 import "./App.css";
 
+import { H1 } from "./components/typography/styles/Typography.styles";
+import { PageWrapper, ContentWrapper } from "./styles/App.styles";
+
 function App() {
   return (
     <div className="App">
-      <AppController />
+      <H1>React Markdown Editor</H1>
+      <PageWrapper>
+        <ContentWrapper>
+          <AppController />
+        </ContentWrapper>
+      </PageWrapper>
     </div>
   );
 }
--- src/controllers/AppController.js
+++ src/controllers/AppController.js
@@ -2,20 +2,15 @@ import React, { useState } from "react";
 
 import MarkdownOutput from "../components/markdown/MarkdownOutput";
 import MarkdownEditor from "../components/markdown/MarkdownEditor";
-import { H1 } from "../components/typography/styles/Typography.styles";
-import { PageWrapper, ContentWrapper } from "./styles/AppController.styles";
 
 const AppController = () => {
   const [editorContent, updateContent] = useState("");
 
   return (
-    <PageWrapper>
-      <H1>React Markdown Editor</H1>
-      <ContentWrapper>
-        <MarkdownEditor textareaHandler={updateContent} />
-        <MarkdownOutput rawContent={editorContent} />
-      </ContentWrapper>
-    </PageWrapper>
+    <>
+      <MarkdownEditor textareaHandler={updateContent} />
+      <MarkdownOutput rawContent={editorContent} />
+    </>
   );
 };
 
