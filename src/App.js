1c344b09dac185ac6f37e1791ae7c5132b7d24dc
Nikhil Nawgiri <the@realest.ninja>
2021-12-29 18:34:52 +0100

    Refactor App

--- src/App.js
+++ src/App.js
@@ -2,16 +2,14 @@ import AppController from "./controllers/AppController";
 import "./App.css";
 
 import { H1 } from "./components/typography/styles/Typography.styles";
-import { PageWrapper, ContentWrapper } from "./styles/App.styles";
+import { PageWrapper } from "./styles/App.styles";
 
 function App() {
   return (
     <div className="App">
-      <H1>React Markdown Editor</H1>
       <PageWrapper>
-        <ContentWrapper>
-          <AppController />
-        </ContentWrapper>
+        <H1>React Markdown Editor</H1>
+        <AppController />
       </PageWrapper>
     </div>
   );
