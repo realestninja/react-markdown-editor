35bee4f348c64c7602ae58f849b66a7a896802f6
Nikhil Nawgiri <the@realest.ninja>
2021-12-29 18:48:20 +0100

    Refactor

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import AppController from "./controllers/AppController";
 import "./App.css";
 
 import { H1 } from "./components/typography/styles/Typography.styles";
 import { PageWrapper } from "./styles/App.styles";
+import MarkdownLogic from "./components/markdown/MarkdownLogic";
 
 function App() {
   return (
     <div className="App">
       <PageWrapper>
         <H1>React Markdown Editor</H1>
-        <AppController />
+        <MarkdownLogic />
       </PageWrapper>
     </div>
   );
--- src/controllers/AppController.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import MarkdownLogic from "../components/markdown/MarkdownLogic";
-
-const AppController = () => (
-  <MarkdownLogic />
-);
-
-export default AppController;
