e28188ef06ffb728568fa296498275bfe6c6cf95
Nikhil Nawgiri <the@realest.ninja>
2021-12-29 18:17:24 +0100

    Improve FileReader

--- src/components/form/FileReader.jsx
+++ src/components/form/FileReader.jsx
@@ -2,13 +2,15 @@ import React, { useRef } from "react";
 import PropTypes from "prop-types";
 import noop from "lodash/noop";
 
+import { HiddenInput } from "./styles/FileReader.styles";
+
 const allowances = {
   markdown: ".md",
   html: "text/html",
   default: null,
 };
 
-const FileReader = ({ callback, allowedFileType }) => {
+const FileReader = ({ callback, allowedFileType, wording, className }) => {
   const inputRef = useRef();
 
   const handleFile = async (val) => {
@@ -17,24 +19,37 @@ const FileReader = ({ callback, allowedFileType }) => {
     inputRef.current.value = null;
   };
 
+  const handleClick = () => {
+    inputRef.current.click();
+  };
+
   return (
-    <input
-      type="file"
-      ref={inputRef}
-      accept={allowances[allowedFileType]}
-      onChange={e => handleFile(e.target.files[0])}
-    />
+    <>
+      <button type="button" onClick={handleClick} className={className}>
+        {wording}
+      </button>
+      <HiddenInput
+        type="file"
+        ref={inputRef}
+        accept={allowances[allowedFileType]}
+        onChange={e => handleFile(e.target.files[0])}
+      />
+    </>
   );
 };
 
 FileReader.propTypes = {
   allowedFileType: PropTypes.string,
   callback: PropTypes.func,
+  wording: PropTypes.string,
+  className: PropTypes.string,
 };
 
 FileReader.defaultProps = {
   allowedFileType: "default",
   callback: noop,
+  wording: "Choose file",
+  className: null,
 };
 
 export default FileReader;
