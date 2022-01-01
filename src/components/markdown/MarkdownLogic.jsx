e30d00e2f14a432a2ba163b1b613c48deb31c755
Nikhil Nawgiri <the@realest.ninja>
2022-01-01 18:44:40 +0100

    Add Html SaveToFileButton to MarkdownLogic

--- src/components/markdown/MarkdownLogic.jsx
+++ src/components/markdown/MarkdownLogic.jsx
@@ -1,29 +1,37 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { NodeHtmlMarkdown } from "node-html-markdown";
+import showdown from "showdown";
 
-import FileReader from "../form/FileReader";
+import ReadFromFileButton from "../form/ReadFromFileButton";
+import SaveToFileButton from "../form/SaveToFileButton";
 import { Wrapper, Row, ButtonRow, StyledMarkdownEditor, StyledMarkdownOutput } from "./styles/MarkdownLogic.styles";
-import FileSaver from "../form/FileSaver";
 
-const nhm = new NodeHtmlMarkdown();
+const converterHtmlToMarkdown = new NodeHtmlMarkdown();
+const converterMarkdownToHtml = new showdown.Converter();
 
 const MarkdownLogic = () => {
-  const [editorContent, updateContent] = useState("");
+  const [editorMdContent, updateContent] = useState("");
+  const [editorHtmlContent, setEditorHtmlContent] = useState("");
+
+  useEffect(() => {
+    setEditorHtmlContent(converterMarkdownToHtml.makeHtml(editorMdContent));
+  }, [editorMdContent]);
 
   const handleNewHtml = (html) => {
-    updateContent(nhm.translate(html));
+    updateContent(converterHtmlToMarkdown.translate(html));
   };
 
   return (
     <Wrapper>
       <ButtonRow>
-        <FileReader callback={handleNewHtml} allowedFileType="html" wording="Import HTML from file" />
-        <FileReader callback={updateContent} allowedFileType="markdown" wording="Import Markdown from file" />
-        <FileSaver content={editorContent} fileType="md" wording="Save markdown to file" />
+        <ReadFromFileButton callback={handleNewHtml} allowedFileType="html" wording="Import HTML from file" />
+        <ReadFromFileButton callback={updateContent} allowedFileType="markdown" wording="Import Markdown from file" />
+        <SaveToFileButton content={editorMdContent} fileType="md" wording="Save markdown to file" />
+        <SaveToFileButton content={editorHtmlContent} fileType="html" wording="Save HTML to file" />
       </ButtonRow>
       <Row>
-        <StyledMarkdownEditor textareaHandler={updateContent} content={editorContent} />
-        <StyledMarkdownOutput rawContent={editorContent} />
+        <StyledMarkdownEditor textareaHandler={updateContent} content={editorMdContent} />
+        <StyledMarkdownOutput rawContent={editorMdContent} />
       </Row>
     </Wrapper>
   );
